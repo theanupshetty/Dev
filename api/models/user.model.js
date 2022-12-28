24e56f3c15056397128436f92ecf619a958642ce
theanupshetty <anupshetty33@gmail.com>
2022-12-28 10:54:33 +0530

    user validation

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,12 +1,62 @@
-const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
-const UserSchema = new mongoose.Schema({
-  name: {
+const { Schema } = mongoose;
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new Schema({
+  email: {
     type: String,
     required: true,
+    createIndexes: { unique: true },
+    unique: true,
+    validate: [
+      {
+        validator: async function (email) {
+          return isEmail(email)
+        },
+        message: 'invalid email',
+        validator: async function (email) {
+          const user = await this.constructor.findOne({ email });
+          if (user) {
+            if (this.id === user.id) {
+              return true;
+            }
+            return false;
+          }
+          return true;
+        },
+        message: 'The specified email address is already in use.'
+      }
+    ]
+  },
+  first_name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  password: { type: String, required: true },
+});
+
+UserSchema.pre('save', async function save(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
   }
 });
 
-const User = mongoose.model("Users", UserSchema);
+UserSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
+
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
--- api/services/user.service.js
+++ api/services/user.service.js
@@ -2,14 +2,14 @@ module.exports = class UserService {
     constructor(userModel) {
         this.userModel = userModel;
     }
-    insertUser(data, callback) {
+    async insertUser(data, callback) {
         // models available throug 'this'
         console.log('mongo log for insert function', data)
-        new this.userModel(data).save(callback)
+        return new this.userModel(data).save(callback)
     }
     getUser = (data, callback) => {
         console.log('mongo log for get function', data)
-        new this.userModel.find(data, callback)
+        this.userModel.findOne(data, callback)
     }
     updateUser = (data, updateData, callback) => {
         console.log('mongo log for update function', data)
